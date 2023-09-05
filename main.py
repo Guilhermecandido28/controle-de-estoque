76c068e74ef4a067c3ef11575ffff665376e6909
Guilhermecandido28 <gnfc28@icloud.com>
2023-09-05 13:13:52 -0300

    criei o primeiro botão do menu de navegação

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from styles.cores import *
 import tkinter as tk
 
 
+
 janela = Tk()
 
 
@@ -11,7 +12,7 @@ class Applicantion():
         self.janela = janela
         self.tela()
         self.frames()
-        self.buttons()        
+        self.buttons()                        
         janela.mainloop()
     def tela(self):
         self.janela.title('Controle fincanceiro')
@@ -19,16 +20,32 @@ class Applicantion():
         self.janela.configure(background= cor5)
     
     def frames(self):
+        self.header = tk.Frame(self.janela, bg=cor3)
+        self.header.place(relx=0, rely=0, relwidth=1, relheight=0.09)
         self.menu = tk.Frame(self.janela, bg=cor4)
-        self.menu.place(relx=0, rely=0, relwidth=0.1, relheight=1)
+        self.menu.place(relx=0, rely=0.09, relwidth=1, relheight=0.05)
+        self.location = tk.Frame(self.janela, bg=cor1)
+        self.location.place(relx=0, rely=0.14, relwidth=1, relheight=0.09)
+        self.search = tk.Frame(self.janela, bg=cor2)
+        self.search.place(relx=0, rely=0.23, relwidth=1, relheight=0.05)
+        self.principal = tk.Frame(self.janela, bg= 'white')
+        self.principal.place(relx=0.01, rely=0.28, relwidth=0.98, relheight=0.67)
+        
 
     def buttons(self):
-        imagem_venda= PhotoImage(file='imagens/vendas.png', master=self.menu)        
-        img_venda = Label(self.menu, text='VENDAS', image=imagem_venda)
-        img_venda.image = imagem_venda
-        self.venda = tk.Button(self.menu, text='VENDAS', image=imagem_venda, bd=0, highlightthickness=0, bg=cor4)
-        self.venda.place(relx=0,rely=0, relwidth=1, relheight=0.1)
-        
+        self.img_cliente = PhotoImage(file='imagens/cliente.png')
+        self.btn_clientes = Button(
+            self.menu, 
+            text='Clientes', 
+            image=self.img_cliente, 
+            compound=LEFT, 
+            bg=cor4, 
+            bd=0,
+            font=('arial 12 bold')
+            
+            )
+        self.btn_clientes.place(relx=0, rely=0, relwidth=0.14, relheight=1)
+ 
 Applicantion()
 
         
