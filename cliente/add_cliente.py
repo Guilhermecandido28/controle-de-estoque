b0974f48d6cee4f24075410e6f4941ecad9181b5
Guilhermecandido28 <gnfc28@icloud.com>
2023-09-13 16:48:35 -0300

    treeview
    Ajustei o add_cliente em um arquivo separado e dei inicio a uma treeview

--- cliente/add_cliente.py
+++ cliente/add_cliente.py
@@ -1,104 +1,40 @@
 from tkinter import *
 from styles.cores import *
 import tkinter as tk
+from tkinter import filedialog
 from formations import *
 from tkinter import ttk
-from upload_antes import carregar_imagem
 from PIL import Image, ImageTk
 from limpar import limpar
 
 
-janela = Tk()
+class AddClient():
+    def __init__(self, frame_pai) -> None:                
+        self.principal = tk.Frame(frame_pai, bg= 'white')
+        self.add_client()
+        self.hist_client()
+        self.buscar_cliente()
+        self.tvw_hist()        
+        self.img = Image.open('imagens/pessoa.png')            
 
-class Applicantion():
-    def __init__(self):
-        self.janela = janela
-        self.tela()
-        self.frames()
-        self.buttons()
-        self.buscar()
-        self.add_client()                               
-        janela.mainloop()
-
-    def tela(self):
-        self.janela.title('Controle fincanceiro')
-        self.janela.geometry('1500x1000')
-        self.janela.configure(background= cor5)
-        janela.minsize(1500, 1000)
-    
-    def frames(self):
-        self.header = tk.Frame(self.janela, bg=cor3)
-        self.header.place(relx=0, rely=0, relwidth=1, relheight=0.09)
-        self.menu = tk.Frame(self.janela, bg=cor4)
-        self.menu.place(relx=0, rely=0.09, relwidth=1, relheight=0.05)
-        self.location = tk.Frame(self.janela, bg=cor1)
-        self.location.place(relx=0, rely=0.14, relwidth=1, relheight=0.09)        
-        self.principal = tk.Frame(self.janela, bg= 'white')
-        self.principal.place(relx=0.01, rely=0.23, relwidth=0.98, relheight=0.67)
-             
-    def buttons(self):
-        #Botão cliente
-        self.img_cliente = PhotoImage(file='imagens/cliente.png')
-        self.btn_clientes = Button(self.menu, text='Clientes', image=self.img_cliente, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_clientes.place(relx=0, rely=0, relwidth=0.14, relheight=1)
-        #botão vendas
-        self.img_vendas = PhotoImage(file='imagens/vendas.png')
-        self.btn_vendas = Button(self.menu, text='Vendas', image=self.img_vendas, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_vendas.place(relx=0.14, rely=0, relwidth=0.14, relheight=1)
-        # Fornecedor
-        self.img_fornecedor = PhotoImage(file='imagens/fornecedor.png')
-        self.btn_fornecedor = Button(self.menu, text='Fornecedor', image=self.img_fornecedor, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_fornecedor.place(relx=0.28, rely=0, relwidth=0.14, relheight=1)
-        #estoque
-        self.img_estoque= PhotoImage(file='imagens/estoque.png')
-        self.btn_estoque = Button(self.menu, text='Estoque', image=self.img_estoque, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_estoque.place(relx=0.42, rely=0, relwidth=0.14, relheight=1)
-        #compras
-        self.img_compras= PhotoImage(file='imagens/compras.png')
-        self.btn_compras = Button(self.menu, text='Compras', image=self.img_compras, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_compras.place(relx=0.56, rely=0, relwidth=0.14, relheight=1)
-        #financeiro
-        self.img_financeiro= PhotoImage(file='imagens/financeiro.png')
-        self.btn_financeiro = Button(self.menu, text='Financeiro', image=self.img_financeiro, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_financeiro.place(relx=0.7, rely=0, relwidth=0.14, relheight=1)
-        #configurações
-        self.img_settings= PhotoImage(file='imagens/settings.png')
-        self.btn_settings = Button(self.menu, text='Configurações', image=self.img_settings, compound=LEFT, bg=cor4, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_settings.place(relx=0.84, rely=0, relwidth=0.14, relheight=1)
-        #home
-        self.img_home = PhotoImage(file='imagens/home.png')
-        self.btn_home = Button(self.header, image=self.img_home, bg=cor3, bd=0, font=('arial 12 bold'), cursor='hand2')
-        self.btn_home.place(relx=0, rely=0, relheight=1, relwidth=0.05)
-        
-    def buscar(self):        
-        self.search = Entry(self.principal, font=('arial 14'), bg=cor5, bd=0)
-        self.search.insert(0, "Procure por...")
-        self.search.bind("<FocusIn>", lambda event: self.search.delete(0, "end"))
-        self.search.place(relx=0, rely=0, relheight=0.1, relwidth=0.8)
-        #botão_pesquisar
-        self.img_search = PhotoImage(file='imagens/search.png')
-        self.btn_search = Button(self.principal, image=self.img_search, bg=cor5, bd=0, cursor='hand2')
-        self.btn_search.place(relx=0.8, rely=0, relheight=0.1, relwidth=0.05)
-        #Botão_imprimir
-        self.img_print = PhotoImage(file='imagens/impressora.png')
-        self.btn_print = Button(self.principal, image=self.img_print, bg=cor5, bd=0, cursor='hand2')
-        self.btn_print.place(relx=0.85, rely=0, relheight=0.1, relwidth=0.05)
-    
     def add_client(self):
+        self.principal.place(relx=0.01, rely=0.23, relwidth=0.98, relheight=0.67)
         #Botão_adicionar_cliente        
         self.btn_addcliente = Button(self.principal, text='ADICIONAR\n Cliente', bg=cor6, compound='center',bd=0, font=('arial 14 bold'), foreground='white', cursor='hand2')
         self.btn_addcliente.place(relx=0.9, rely=0, relheight=0.1, relwidth=0.1)
         #título 
         self.titulo = Label(self.principal, text='Ficha de Cadastro de Clientes', bg='white', font=('arial 16 bold'))
-        self.titulo.place(relx=0., rely=0.1, relwidth=0.5, relheight=0.1)
-        #imagem        
-        self.icone_cliente = PhotoImage(file='imagens/pessoa.png')        
-        self.label_cliente = Label(self.principal, image=self.icone_cliente, bg='white')        
-        self.label_cliente.place(relx=0.01, rely=0.12)
         self.linha = Frame(self.principal, bg=cor5)
         self.linha.place(relx=0.146, rely=0.17, relwidth=0.525, relheight=0.004)
-        botao_upload = tk.Button(janela, text="Upload de foto", command=lambda: carregar_imagem(self.label_cliente), font=('arial 12 bold'), background='green', foreground='white', cursor='hand2')
-        botao_upload.place(relx=0.030, rely=0.52)
+        self.titulo.place(relx=0., rely=0.1, relwidth=0.5, relheight=0.1)
+        #imagem
+        self.my_canvas = Canvas(self.principal, bd=0, highlightthickness=0, relief='ridge')
+        self.my_canvas.place(relx=0, rely=0.1, relheight=.34, relwidth=.14)
+              
+        self.my_canvas.bind('<Configure>', self.resizer)
+        botao_upload = tk.Button(self.principal, command= self.upload , text="Upload de foto", font=('arial 12 bold'), background='green', foreground='white', cursor='hand2')
+        botao_upload.place(relx=0, rely=0.44, relwidth=.14, relheight=0.04)
+        
         
         # Entrys
             #nome
@@ -180,8 +116,51 @@ class Applicantion():
         self.btn_limpar_endereco.place(relx=.600, rely=.810, relheight=0.05, relwidth= 0.07)        
         self.btn_limpar_cadastro = Button(self.principal, text=' Limpar', image=self.img_limpar, compound=LEFT, bg=cor4, font=('arial 12 bold'), command= lambda:limpar([self.e_nome, self.e_sobrenome, self.e_cpf, self.e_celular, self.e_email, self.e_comment]), cursor='hand2')
         self.btn_limpar_cadastro.place(relx=.600, rely=.505, relheight=0.05, relwidth= 0.07)
+
+    def hist_client(self):
+        self.hist_titulo = Label(self.principal, text='Clientes Cadastrados', bg='white', font=('arial 16 bold'), foreground=cor5)
+        self.hist_linha = Frame(self.principal, bg=cor5)
+        self.hist_linha.place(relx=0.689, rely=0.17, relwidth=0.30, relheight=0.004)
+        self.hist_titulo.place(relx=.51, rely=0.1, relwidth=0.5, relheight=0.1)
+
+    def tvw_hist(self):               
+        self.tvw_hist = ttk.Treeview(self.principal, columns=("Data", "Nome", "Valor Gasto"))
+        self.tvw_hist.column("#0", width=25, minwidth=25)        
+        self.tvw_hist.column("Data", anchor=W, width=120)
+        self.tvw_hist.column("Nome", anchor=W, width=200, minwidth=120)
+        self.tvw_hist.column("Valor Gasto", anchor=CENTER, width=80, minwidth=25)
+        self.tvw_hist.place(relx=0.689, rely=0.19, relheight=.7, relwidth=.3)
+        self.tvw_hist.heading("#0", text="ID", anchor=CENTER)
+        self.tvw_hist.heading("#1", text="Data", anchor=CENTER)
+        self.tvw_hist.heading("#2", text="Nome", anchor=CENTER)
+        self.tvw_hist.heading("#3", text="Valor Gasto", anchor=CENTER)
         
 
-          
+    def buscar_cliente(self):
+        self.search = Entry(self.principal, font=('arial 14'), bg=cor5, bd=0)
+        self.search.insert(0, "Procure por...")
+        self.search.bind("<FocusIn>", lambda event: self.search.delete(0, "end"))
+        self.search.place(relx=0, rely=0, relheight=0.1, relwidth=0.8)
+        #botão_pesquisar
+        self.img_search = PhotoImage(file='imagens/search.png')
+        self.btn_search = Button(self.principal, image=self.img_search, bg=cor5, bd=0, cursor='hand2')
+        self.btn_search.place(relx=0.8, rely=0, relheight=0.1, relwidth=0.05)
+        #Botão_imprimir
+        self.img_print = PhotoImage(file='imagens/impressora.png')
+        self.btn_print = Button(self.principal, image=self.img_print, bg=cor5, bd=0, cursor='hand2')
+        self.btn_print.place(relx=0.85, rely=0, relheight=0.1, relwidth=0.05) 
+   
+    def resizer(self,e):
+        global resized_img, new_img       
+        resized_img = self.img.resize((e.width, e.height))        
+        new_img = ImageTk.PhotoImage(resized_img)
+        self.my_canvas.create_image(0,0, image=new_img, anchor='nw')
 
-Applicantion()
+    def upload(self):        
+        self.filename = filedialog.askopenfilename(title="Selecione uma foto", filetypes=[("Imagens", "*.jpg *.png *.bmp")])        
+        self.img = Image.open(self.filename)
+        largura = self.my_canvas.winfo_width()
+        altura = self.my_canvas.winfo_height()
+        resized_img2 = self.img.resize((largura, altura))
+        self.img_tk = ImageTk.PhotoImage(resized_img2)
+        self.my_canvas.create_image(0,0, image=self.img_tk, anchor='nw') 
--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from tkinter import *
 from styles.cores import *
 import tkinter as tk
-from tkinter import filedialog
 from formations import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from limpar import limpar
+from cliente.add_cliente import AddClient
 
 
 janela = Tk()
 
-class Applicantion():
+class Applicantion(AddClient):
     def __init__(self):
         self.janela = janela
         self.img = Image.open('imagens/pessoa.png')
         self.tela()
         self.frames()
-        self.buttons()
-        self.buscar()
-        self.add_client()                               
+        self.buttons()       
+        AddClient(self.janela)                      
         janela.mainloop()
 
     def tela(self):
@@ -34,8 +33,7 @@ class Applicantion():
         self.menu.place(relx=0, rely=0.09, relwidth=1, relheight=0.05)
         self.location = tk.Frame(self.janela, bg=cor1)
         self.location.place(relx=0, rely=0.14, relwidth=1, relheight=0.09)        
-        self.principal = tk.Frame(self.janela, bg= 'white')
-        self.principal.place(relx=0.01, rely=0.23, relwidth=0.98, relheight=0.67)
+        
              
     def buttons(self):
         #Botão cliente
@@ -70,135 +68,7 @@ class Applicantion():
         self.img_home = PhotoImage(file='imagens/home.png')
         self.btn_home = Button(self.header, image=self.img_home, bg=cor3, bd=0, font=('arial 12 bold'), cursor='hand2')
         self.btn_home.place(relx=0, rely=0, relheight=1, relwidth=0.05)
-        
-    def buscar(self):        
-        self.search = Entry(self.principal, font=('arial 14'), bg=cor5, bd=0)
-        self.search.insert(0, "Procure por...")
-        self.search.bind("<FocusIn>", lambda event: self.search.delete(0, "end"))
-        self.search.place(relx=0, rely=0, relheight=0.1, relwidth=0.8)
-        #botão_pesquisar
-        self.img_search = PhotoImage(file='imagens/search.png')
-        self.btn_search = Button(self.principal, image=self.img_search, bg=cor5, bd=0, cursor='hand2')
-        self.btn_search.place(relx=0.8, rely=0, relheight=0.1, relwidth=0.05)
-        #Botão_imprimir
-        self.img_print = PhotoImage(file='imagens/impressora.png')
-        self.btn_print = Button(self.principal, image=self.img_print, bg=cor5, bd=0, cursor='hand2')
-        self.btn_print.place(relx=0.85, rely=0, relheight=0.1, relwidth=0.05)
-
-    def upload(self):
-        self.filename = filedialog.askopenfilename(title="Selecione uma foto", filetypes=[("Imagens", "*.jpg *.png *.bmp")])        
-        self.img = Image.open(self.filename)
-        largura = self.my_canvas.winfo_width()
-        altura = self.my_canvas.winfo_height()
-        resized_img2 = self.img.resize((largura, altura))
-        self.img_tk = ImageTk.PhotoImage(resized_img2)
-        self.my_canvas.create_image(0,0, image=self.img_tk, anchor='nw')
-
-    def resizer(self,e):
-        global resized_img, new_img       
-        resized_img = self.img.resize((e.width, e.height))        
-        new_img = ImageTk.PhotoImage(resized_img)
-        self.my_canvas.create_image(0,0, image=new_img, anchor='nw')
-
-    def add_client(self):
-        #Botão_adicionar_cliente        
-        self.btn_addcliente = Button(self.principal, text='ADICIONAR\n Cliente', bg=cor6, compound='center',bd=0, font=('arial 14 bold'), foreground='white', cursor='hand2')
-        self.btn_addcliente.place(relx=0.9, rely=0, relheight=0.1, relwidth=0.1)
-        #título 
-        self.titulo = Label(self.principal, text='Ficha de Cadastro de Clientes', bg='white', font=('arial 16 bold'))
-        self.linha = Frame(self.principal, bg=cor5)
-        self.linha.place(relx=0.146, rely=0.17, relwidth=0.525, relheight=0.004)
-        self.titulo.place(relx=0., rely=0.1, relwidth=0.5, relheight=0.1)
-        #imagem
-        self.my_canvas = Canvas(self.principal, bd=0, highlightthickness=0, relief='ridge')
-        self.my_canvas.place(relx=0, rely=0.1, relheight=.34, relwidth=.14)       
-        self.my_canvas.bind('<Configure>', self.resizer)
-        botao_upload = tk.Button(self.principal, command= self.upload , text="Upload de foto", font=('arial 12 bold'), background='green', foreground='white', cursor='hand2')
-        botao_upload.place(relx=0, rely=0.44, relwidth=.14, relheight=0.04)
-        
-        
-        # Entrys
-            #nome
-        self.title_nome = Label(self.principal, text='NOME:', font=('arial 12'), foreground= cor4, bg='white')
-        self.title_nome.place(relx=0.148, rely=0.195)
-        self.e_nome = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_nome.place(relx=0.150, rely=0.24, relwidth=0.25, relheight=0.04)
-        placeholder_nome(self.e_nome)
-            #sobrenome
-        self.title_sobrenome = Label(self.principal, text='SOBRENOME:', font=('arial 12'), foreground= cor4, bg='white')
-        self.title_sobrenome.place(relx=0.416, rely=0.195)
-        self.e_sobrenome = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_sobrenome.place(relx=0.42, rely=0.24, relwidth=0.25, relheight=0.04)
-        placeholder_sobrenome(self.e_sobrenome)
-            #CPF
-        self.title_cpf = Label(self.principal, text='CPF:', font=('arial 12'), foreground= cor4, bg='white')
-        self.title_cpf.place(relx=0.148, rely=0.295)
-        self.e_cpf = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_cpf.place(relx=0.150, rely=0.34, relwidth=0.15, relheight=0.04)
-        placeholder_cpf(self.e_cpf)
-            #celular
-        self.title_celular = Label(self.principal, text="CELULAR:", font=('arial 12'), foreground=cor4, bg='white')
-        self.title_celular.place(relx= 0.316 , rely=0.295)
-        self.e_celular = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_celular.place(relx=0.32, rely=0.34, relwidth=0.15, relheight=0.04)
-        placeholder_celular(self.e_celular)
-            #email
-        self.title_email = Label(self.principal, text="EMAIL:", font=('arial 12'), foreground=cor4, bg='white')
-        self.title_email.place(relx= 0.49 , rely=0.295) 
-        self.e_email = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_email.place(relx=0.49, rely=0.34, relwidth=0.18, relheight=0.04)
-        placeholder_email(self.e_email)
-            #cometário
-        self.title_comment = Label(self.principal, text="OBS:", font=('arial 12'), foreground=cor4, bg='white')
-        self.title_comment.place(relx=0.148, rely=0.395)
-        self.e_comment = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_comment.place(relx=0.150, rely=0.44, relwidth=0.52, relheight=0.04)
-        # endereço
-        self.linha2 = Frame(self.principal, bg=cor5)
-        self.linha2.place(relx=0.01, rely=0.56, relwidth=0.66, relheight=0.004)
-        self.titulo_endereco = Label(self.principal, text="ENDEREÇO", font=('arial 14'), foreground=cor4, bg='white')
-        self.titulo_endereco.place(relx=0.01, rely=0.51)
-            #CEP
-        self.title_cep = Label(self.principal, text="CEP:", font=('arial 12'), foreground=cor4, bg='white')
-        self.title_cep.place(relx=0.01, rely=0.605)
-        self.e_cep = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_cep.place(relx=0.01, rely=0.65, relwidth=0.08, relheight=0.04)  
-        placeholder_cep(self.e_cep)
-            #RUA
-        self.title_rua = Label(self.principal, text="RUA:", font=('arial 14'), foreground=cor4, bg='white')
-        self.title_rua.place(relx=.125, rely= .605 )
-        self.e_rua = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_rua.place(relx=0.125, rely=0.65, relwidth=0.38, relheight=0.04)
-            #N°
-        self.title_numero = Label(self.principal, text="NÚMERO:", font=('arial 14'), foreground=cor4, bg='white')
-        self.title_numero.place(relx=.545, rely= .605 )
-        self.e_numero = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_numero.place(relx=0.545, rely=0.65, relwidth=0.124, relheight=0.04)
-            #BAIRRO
-        self.title_bairro = Label(self.principal, text="BAIRRO:", font=('arial 14'), foreground=cor4, bg='white')
-        self.title_bairro.place(relx=.01, rely= .705 )
-        self.e_bairro = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_bairro.place(relx=0.01, rely=0.75, relwidth=0.20, relheight=0.04) 
-            #CIDADE
-        self.title_cidade = Label(self.principal, text="CIDADE:", font=('arial 14'), foreground=cor4, bg='white')
-        self.title_cidade.place(relx=.24, rely= .705 )
-        self.e_cidade = Entry(self.principal, bg=cor4, font=('arial 12'), bd=0)
-        self.e_cidade.place(relx=0.24, rely=0.75, relwidth=0.263, relheight=0.04)
-            #ESTADO
-        estados = ttk.Combobox(self.principal, font=('arial 12'), takefocus=True, state='readonly')
-        estados['values'] = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
-        estados.current(24)  # Pré-seleciona o estado de São Paulo
-        estados.place(relx=0.545, rely=0.75, relwidth=0.124, relheight=0.04)      
-        self.title_estado = Label(self.principal, text="ESTADO:", font=('arial 14'), foreground=cor4, bg='white')
-        self.title_estado.place(relx=.545, rely= .705 )
-        #botão limpar
-        self.img_limpar = PhotoImage(file='imagens/vassoura.png')
-        self.btn_limpar_endereco = Button(self.principal, text=' Limpar', image=self.img_limpar, compound=LEFT, bg=cor4, font=('arial 12 bold'), command=lambda: limpar([self.e_cidade, self.e_bairro, self.e_numero, self.e_rua, self.e_cep]), cursor='hand2')
-        self.btn_limpar_endereco.place(relx=.600, rely=.810, relheight=0.05, relwidth= 0.07)        
-        self.btn_limpar_cadastro = Button(self.principal, text=' Limpar', image=self.img_limpar, compound=LEFT, bg=cor4, font=('arial 12 bold'), command= lambda:limpar([self.e_nome, self.e_sobrenome, self.e_cpf, self.e_celular, self.e_email, self.e_comment]), cursor='hand2')
-        self.btn_limpar_cadastro.place(relx=.600, rely=.505, relheight=0.05, relwidth= 0.07)
-        
-
-          
-
+           
 Applicantion()
+
+
